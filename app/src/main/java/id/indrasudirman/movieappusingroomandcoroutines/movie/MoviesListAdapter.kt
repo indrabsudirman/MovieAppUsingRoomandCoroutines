30786a5a5c84866804fbd80b192aad25d7397873
unknown <indra.indrabayu@gmail.com>
2021-12-29 15:35:32 +0700

    Fragment Movies

--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListAdapter.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListAdapter.kt
@@ -2,11 +2,18 @@ package id.indrasudirman.movieappusingroomandcoroutines.movie
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import id.indrasudirman.movieappusingroomandcoroutines.database.Movie
+import id.indrasudirman.movieappusingroomandcoroutines.database.MovieDatabase
 import id.indrasudirman.movieappusingroomandcoroutines.databinding.ItemListDirectorBinding
 import id.indrasudirman.movieappusingroomandcoroutines.databinding.ItemListMovieBinding
+import id.indrasudirman.movieappusingroomandcoroutines.movie.MovieSaveDialogFragment.Companion.newInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MoviesListAdapter(private val parent: Fragment) : RecyclerView.Adapter<MoviesListAdapter.MoviesViewHolder>() {
 
@@ -27,10 +34,36 @@ class MoviesListAdapter(private val parent: Fragment) : RecyclerView.Adapter<Mov
     }
 
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        movieList?.let { list ->
+            val movie = list[position]
+            holder.itemListMovieBinding.tvMovieTitle.text = movie.title
+            runBlocking {
+                val directorFullName = withContext(Dispatchers.Default) {
+                    getDirectorFullName(movie)
+                }
+
+                holder.itemListMovieBinding.tvMovieDirectorFullName.text = directorFullName ?: ""
+                holder.itemListMovieBinding.root.setOnClickListener {
+                    val dialogFragment: DialogFragment = newInstance(movie.title, directorFullName)
+                    dialogFragment.setTargetFragment(parent, 99)
+                    dialogFragment.show(
+                        (parent.activity as AppCompatActivity).supportFragmentManager,
+                        MovieSaveDialogFragment.TAG_DIALOG_MOVIE_SAVE
+                    )
+                }
+            }
+        }
+    }
+
+    private suspend fun getDirectorFullName(movie: Movie) : String? {
+        return MovieDatabase.getDatabase(parent.requireContext()).directorDao().findDirectorById(movie.directorId)?.fullName
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return if (movieList == null) {
+            0
+        } else {
+            movieList!!.size
+        }
     }
 }
--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListFragment.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListFragment.kt
@@ -1,4 +1,30 @@
 package id.indrasudirman.movieappusingroomandcoroutines.movie
 
-class MoviesListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.indrasudirman.movieappusingroomandcoroutines.database.Movie
+
+class MoviesListFragment : Fragment() {
+
+    private lateinit var moviesListAdapter: MoviesListAdapter
+    private lateinit var moviesViewModel: MoviesViewModel
+    private lateinit var moviesList: List<Movie>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initData()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun initData() {}
 }
