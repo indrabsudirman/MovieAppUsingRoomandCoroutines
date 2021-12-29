f70e86273a583f67efb8e503cef6b957d95c733b
Indra Sudirman <indrabsudirman@gmail.com>
2021-12-29 13:05:47 +0700

    MoviesListAdapter

--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MovieSaveDialogFragment.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MovieSaveDialogFragment.kt
@@ -3,8 +3,15 @@ package id.indrasudirman.movieappusingroomandcoroutines.movie
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.DialogFragment
 import id.indrasudirman.movieappusingroomandcoroutines.R
+import id.indrasudirman.movieappusingroomandcoroutines.database.Director
+import id.indrasudirman.movieappusingroomandcoroutines.database.Movie
+import id.indrasudirman.movieappusingroomandcoroutines.database.MovieDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MovieSaveDialogFragment : DialogFragment() {
 
@@ -23,6 +30,62 @@ class MovieSaveDialogFragment : DialogFragment() {
         val view = requireActivity().layoutInflater.inflate(R.layout.dialog_movie, null)
         val movieEditText = view.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.et_movie_title)
         val movieDirectorEditText = view.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.et_movie_director_full_name)
+
+        if (movieTitleExtra != null) {
+            movieEditText.setText(movieTitleExtra)
+            movieEditText.setSelection(movieTitleExtra!!.length)
+        }
+        alertDialogBuilder.setView(view)
+            .setTitle(getString(R.string.dialog_movie_title))
+            .setPositiveButton(R.string.save) { _,_ ->
+                GlobalScope.launch (Dispatchers.IO) {saveMovie(movieEditText.text.toString(), movieDirectorEditText.text.toString())  }
+            }
+            .setNegativeButton(R.string.cancel) {dialog, _, -> dialog.cancel()}
+
+        return alertDialogBuilder.create()
+    }
+
+    private suspend fun saveMovie(movieTitle: String, movieDirectionFullName: String) {
+        if (TextUtils.isEmpty(movieTitle) || TextUtils.isEmpty(movieDirectionFullName)) {
+            return
+        }
+        val directorDao = MovieDatabase.getDatabase(requireContext()).directorDao()
+        val movieDao = MovieDatabase.getDatabase(requireContext()).movieDao()
+        var directorId: Long = -1L
+        if (movieDirectorFullNameExtra != null) {
+            //Clicked on item row -> update
+            val directorToUpdate = directorDao.findDirectorByName(movieDirectorFullNameExtra)
+            if (directorToUpdate != null) {
+                directorId = directorToUpdate.id
+                if (directorToUpdate.fullName != movieDirectionFullName) {
+                    directorToUpdate.fullName = movieDirectionFullName
+                    directorDao.update(directorToUpdate)
+                }
+            }
+        } else {
+            // we need director id for movie object; in case director is already in DB
+            //insert() would return -1, so we manually check is it exists and get
+            //the id of already save director
+            val newDirector = directorDao.findDirectorByName(movieDirectionFullName)
+            directorId = newDirector?.id ?: directorDao.insert(Director(fullName = movieDirectionFullName))
+        }
+        if (movieTitleExtra != null) {
+            //clicked on item row -> update
+            val movieToUpdate = movieDao.findMovieByTitle(movieTitleExtra!!)
+            if (movieToUpdate != null) {
+                if (movieToUpdate.title != movieTitle) {
+                    movieToUpdate.title = movieTitle
+                    if (directorId != -1L) {
+                        movieToUpdate.directorId = directorId
+                    }
+                    movieDao.update(movieToUpdate)
+                }
+            }
+        } else {
+            //we can many movies with same title but different director
+            movieDao.insert(Movie(title = movieTitle, directorId = directorId))
+        }
+
     }
 
     companion object {
--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListAdapter.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListAdapter.kt
@@ -1,4 +1,36 @@
 package id.indrasudirman.movieappusingroomandcoroutines.movie
 
-class MoviesListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import id.indrasudirman.movieappusingroomandcoroutines.database.Movie
+import id.indrasudirman.movieappusingroomandcoroutines.databinding.ItemListDirectorBinding
+import id.indrasudirman.movieappusingroomandcoroutines.databinding.ItemListMovieBinding
+
+class MoviesListAdapter(private val parent: Fragment) : RecyclerView.Adapter<MoviesListAdapter.MoviesViewHolder>() {
+
+    private val layOutInflater: LayoutInflater = LayoutInflater.from(parent.requireContext())
+    private var movieList: List<Movie>? = null
+
+    fun setMovieList(movieList: List<Movie>?) {
+        this.movieList = movieList
+        notifyDataSetChanged()
+    }
+
+    inner class MoviesViewHolder (val itemListMovieBinding: ItemListMovieBinding): RecyclerView.ViewHolder(itemListMovieBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        val itemListMovieBinding = ItemListMovieBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return MoviesViewHolder(itemListMovieBinding)
+    }
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
