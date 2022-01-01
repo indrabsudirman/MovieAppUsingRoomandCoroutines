deac07cb516698ce437888a9521ba511a7a6b27e
unknown <indra.indrabayu@gmail.com>
2022-01-01 23:02:57 +0700

    MoviesListFragment

--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListFragment.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesListFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.indrasudirman.movieappusingroomandcoroutines.R
 import id.indrasudirman.movieappusingroomandcoroutines.database.Movie
+import id.indrasudirman.movieappusingroomandcoroutines.database.MovieDatabase
+import id.indrasudirman.movieappusingroomandcoroutines.databinding.FragmentMoviesBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MoviesListFragment : Fragment() {
 
     private lateinit var moviesListAdapter: MoviesListAdapter
     private lateinit var moviesViewModel: MoviesViewModel
     private lateinit var moviesList: List<Movie>
+    private var _moviesBinding: FragmentMoviesBinding? = null
+    private val moviesBinding get() = _moviesBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +34,30 @@ class MoviesListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _moviesBinding = FragmentMoviesBinding.inflate(inflater, container, false)
+        val view = moviesBinding.root
+        initView()
+
+        return view
     }
 
-    private fun initData() {}
+    private fun initData() {
+
+    }
+
+    private suspend fun getDirectionFullName(movie: Movie): String? {
+        return MovieDatabase.getDatabase(requireContext()).directorDao().findDirectorById(movie.directorId)?.fullName
+    }
+
+    private fun initView() {
+        val recyclerView: RecyclerView = moviesBinding.recyclerviewMovies
+        moviesListAdapter = MoviesListAdapter(this)
+        recyclerView.adapter = moviesListAdapter
+        recyclerView.addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    fun removeData() {
+//        GlobalScope.launch (Dispatchers.IO) { moviesViewModel.de }
+    }
 }
--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesViewModel.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/movie/MoviesViewModel.kt
@@ -1,4 +1,33 @@
 package id.indrasudirman.movieappusingroomandcoroutines.movie
 
-class MoviesViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import id.indrasudirman.movieappusingroomandcoroutines.database.*
+
+class MoviesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val movieDao: MovieDao = MovieDatabase.getDatabase(application).movieDao()
+    private val directorDao: DirectorDao = MovieDatabase.getDatabase(application).directorDao()
+
+    val moviesList: LiveData<List<Movie>>
+    val directorList: LiveData<List<Director>>
+
+    init {
+        moviesList = movieDao.allMovies
+        directorList = directorDao.allDirectors
+    }
+
+    suspend fun insert(vararg movies: Movie) {
+        movieDao.insert(*movies)
+    }
+
+    suspend fun update(movie: Movie) {
+        movieDao.update(movie)
+    }
+
+    suspend fun deleteAll() {
+        movieDao.deleteAll()
+    }
+
 }
