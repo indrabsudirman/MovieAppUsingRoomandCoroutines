5ab1b7a84801887709f4320c3ec95aa4e17b79a3
Indra Sudirman <indrabsudirman@gmail.com>
2022-01-03 01:33:03 +0700

    Content_main.xml

--- app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/MainActivity.kt
+++ app/src/main/java/id/indrasudirman/movieappusingroomandcoroutines/MainActivity.kt
@@ -2,11 +2,24 @@ package id.indrasudirman.movieappusingroomandcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.Nullable
+import id.indrasudirman.movieappusingroomandcoroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //View Binding
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+
+    }
+
+    private fun initToolbar(@Nullable title: String?) {
+
     }
 }
 
